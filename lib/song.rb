c30ec139008ae9a4ce5a3ffe02c4678e5d1f23ff
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-23 22:59:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
 
   def self.find_by_name(name)
-    @@all.detect {|song| song.name == "#{name}"}
+    @@all.find {|song| song.name == "#{name}"}
   end
 
   def slef.find_or_create_by_name(name)
