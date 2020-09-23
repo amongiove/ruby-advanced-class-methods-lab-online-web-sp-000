f6f83184006bb76d94235a08a725ea3a2e784bd7
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-23 23:16:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
 
   def self.new_from_filename(filename)
       song = self.new
-      splie_filename = filename.chomp(".mp3").split(" - ")
+      split_filename = filename.chomp(".mp3").split(" - ")
       song.name = split_filename[1]
       song.name = split_filename[0]
       song
